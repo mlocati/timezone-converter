ff5bf89d5468355f1243565013404d28b99f6792
cahueya <cahueya@users.noreply.github.com>
2019-11-16 11:31:43 +0100

    Add de-DE.ts

---
+++ src/i18n/de-DE.ts
@@ -0,0 +1,14 @@
+export default {
+  'Time Zone Converter': 'Zeitzonenkonverter',
+  'Source time zone': 'Ausgangszeitzone',
+  'Your time zone': 'Ihre Zeitzone',
+  'Set source date/time': 'Ausgangs-Zeit/Datum eintstellen',
+  '- others -': '- andere -',
+  'Choose a time zone': 'Wählen Sie eine Zeitzone',
+  'Custom time zone': 'Persönliche Zeitzone',
+  'Edit': 'Bearbeiten',
+  'Remove': 'Entfernen',
+  'Copy URL': 'URL kopieren',
+  'Copy failed: {reason}': 'Kopieren fehlgeschlagen: {Grund}',
+  'unknown reason': 'Unbekannter Grund'
+}
