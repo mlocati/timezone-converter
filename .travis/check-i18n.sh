2009a3f6d04c230296b62dc90082f640d7d4537b
Michele Locati <michele@locati.it>
2019-11-19 18:20:29 +0100

    Switch to GitHub Actions

--- .travis/check-i18n.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-npm run vue-i18n-extract -- report -v './src/**/*.vue' -l './src/i18n/*.json' -o i18n-report.json
-
-grep -q '{"missingKeys":\[\],"unusedKeys":\[\]}' i18n-report.json
